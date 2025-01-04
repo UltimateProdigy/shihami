cb96caf22e57f07d22091fcf082612f0237ca0ba
Akinola Ayobami <ayobamiakinola84@gmail.com>
2025-01-04 22:41:08 +0100

    feat: Scroll Reveal

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,5 @@
+import { useEffect } from "react";
+import ScrollReveal from "scrollreveal";
 import "./App.css";
 import Hero from "./features/hero";
 import Support from "./features/support";
@@ -5,12 +7,34 @@ import Instructions from "./features/instructions";
 import { Collections } from "./features/collections";
 
 function App() {
+	useEffect(() => {
+		const sr = ScrollReveal({
+			origin: "bottom",
+			distance: "80px",
+			duration: 2000,
+			reset: false,
+		});
+		sr.reveal(".hero-section", {});
+		sr.reveal(".support-section", { delay: 200 });
+		sr.reveal(".instructions-section", { delay: 300 });
+		sr.reveal(".collections-section", { delay: 400 });
+		return () => sr.destroy();
+	}, []);
+
 	return (
 		<div className="px-[50px]">
-			<Hero />
-			<Support />
-			<Instructions />
-			<Collections />
+			<div className="hero-section">
+				<Hero />
+			</div>
+			<div className="support-section">
+				<Support />
+			</div>
+			<div className="instructions-section">
+				<Instructions />
+			</div>
+			<div className="collections-section">
+				<Collections />
+			</div>
 		</div>
 	);
 }
